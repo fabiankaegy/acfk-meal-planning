f9a6a5678fb6638649e5b8f91bc997ccb6776c0e
fabiankaegy <fabian@arvernus.info>
2019-02-15 23:11:01 +0100

    Testing Action

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Testing Deployment 2
+            Testing Deployment 5
           </p>
         </header>
       </div>
