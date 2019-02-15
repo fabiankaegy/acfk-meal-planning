4dc8e93ff21f870066a08429664c757fd5bf0685
fabiankaegy <fabian@arvernus.info>
2019-02-16 00:42:02 +0100

    Add Key to Day

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -7,8 +7,8 @@ const Day = props => {
             <header className="header">
                 <h2>{props.title}</h2></header>
             <div className="recipes">
-                {props.recipes.map( recipe => (
-                    <span>A Recipe will go here</span>
+                {props.recipes.map( (recipe, key) => (
+                    <span key={key}>A Recipe will go here</span>
                 ) )}
             </div>
         </div>
