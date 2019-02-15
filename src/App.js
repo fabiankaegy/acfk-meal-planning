6ff941b268501ad6de6a520d89d792e392b2f3c4
fabiankaegy <fabian@arvernus.info>
2019-02-15 22:15:21 +0100

    Testing Automatic Deployment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Meal Planning App
+            Testing
           </p>
         </header>
       </div>
