bd7e0edd1f506b13a3b4a875602891c31f673437
fabiankaegy <fabian@arvernus.info>
2019-02-15 22:06:45 +0100

    Rendering Week Component to the Page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Week from './components/Week';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Meal Planning App
-          </p>
-        </header>
+        <Week />
       </div>
     );
   }
