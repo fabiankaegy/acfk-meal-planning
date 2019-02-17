68f39c292af4059761f01d531ac1b86625fb364b
fabiankaegy <fabian@arvernus.info>
2019-02-17 16:09:05 +0100

    Updating Tests

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -20,13 +20,17 @@ const Day = props => {
       <header className="header">
         <h2>{props.title}</h2>
       </header>
-      <div className="recipes">
+      <div className="recipes" data-testid="recipes">
         {recipes.map((recipe, key) => (
           <span key={key}>A Recipe will go here</span>
         ))}
-        <button onClick={addRecipe}>Add Recipe</button>
-        <button onClick={removeRecipe}>Remove Recipe</button>
       </div>
+      <button onClick={addRecipe} data-testid="add-recipe-button">
+        Add Recipe
+      </button>
+      <button onClick={removeRecipe} data-testid="remove-recipe-button">
+        Remove Recipe
+      </button>
     </div>
   );
 };
--- src/components/Day/test.js
+++ src/components/Day/test.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render, cleanup } from "react-testing-library";
+import { render, cleanup, fireEvent } from "react-testing-library";
 import Day from "./index";
 
 describe("Day Component", function() {
   let wrapper;
   beforeEach(function() {
     wrapper = render(<Day title="Title" />);
+  });
+
+  afterEach(() => {
+    cleanup(wrapper);
   });
 
   it("renders without crashing", () => {
@@ -17,5 +21,28 @@ describe("Day Component", function() {
 
   it("matches snapshot", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("adds recipe after add recipe button is clicked", () => {
+    const { getByTestId } = wrapper;
+    const addRecipeButton = getByTestId("add-recipe-button");
+    const recipes = getByTestId("recipes").childNodes;
+
+    const lengthBefore = recipes.length;
+    fireEvent.click(addRecipeButton);
+    expect(recipes.length).toBe(lengthBefore + 1);
+  });
+
+  it("removes recipe after remove recipe button is clicked", () => {
+    const { getByTestId } = wrapper;
+    const addRecipeButton = getByTestId("add-recipe-button");
+    const removeRecipeButton = getByTestId("remove-recipe-button");
+    const recipes = getByTestId("recipes").childNodes;
+
+    fireEvent.click(addRecipeButton);
+    expect(recipes.length).toBe(1);
+
+    fireEvent.click(removeRecipeButton);
+    expect(recipes.length).toBe(0);
   });
 });
