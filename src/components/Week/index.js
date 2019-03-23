3662d01e66cb822294d1b0f9185a70e15fb019d2
fabiankaegy <fabian@arvernus.info>
2019-03-23 15:19:08 +0100

    Adding basic implementation of the dav / week view

--- src/components/Week/index.js
+++ src/components/Week/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Day from '../Day';
 import './style.scss';
 
 const Week = props => {
 	const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+	const [activeDay, setActiveDay] = useState({ name: days[0], key: 0 });
+	const [isDesktop, setIsDesktop] = useState(false);
 
-	return (
+	useEffect(() => {
+		window.addEventListener('resize', event => {
+			window.innerWidth > 800 ? setIsDesktop(true) : setIsDesktop(false);
+		});
+
+		return () => {
+			window.removeEventListener('resize', event => {
+				window.innerWidth > 800 ? setIsDesktop(true) : setIsDesktop(false);
+			});
+		};
+	});
+
+	return isDesktop ? (
 		<section className="week">
 			{days.map((name, key) => (
 				<Day key={key} title={name} />
 			))}
+		</section>
+	) : (
+		<section className="mobile-calendar">
+			{days.map((name, key) => (
+				<button
+					onClick={() => {
+						setActiveDay({ name: name, key: key });
+					}}
+				>
+					{name}
+				</button>
+			))}
+			<Day title={activeDay.name} key={activeDay.key} />
 		</section>
 	);
 };
