0dad42184c43471a221866087f6613a738d27bd3
fabiankaegy <fabian@arvernus.info>
2019-03-23 23:14:07 +0100

    Add Document title changes for accesibillity

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotFound from '../NotFound';
 import Home from '../Home';
 import Wrapper from '../Wrapper';
+import DocumentTitle from 'react-document-title';
 
 const Router = props => {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route path="/recipe/:id" component={Wrapper} />
-				<Route component={NotFound} />
-			</Switch>
-		</BrowserRouter>
+		<Fragment>
+			<DocumentTitle title="ACFK Mealplaning" />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/recipe/:id" component={Wrapper} />
+					<Route component={NotFound} />
+				</Switch>
+			</BrowserRouter>
+		</Fragment>
 	);
 };
 
