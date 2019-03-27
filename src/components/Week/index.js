c0fe7ddccb6600bb3ee4bd56f49592f019eca22d
Amber <a.77@me.com>
2019-03-26 20:24:22 -0700

    Added Modal to Recipe day list
    Added modal from @reach UI and modified recipe listing specific styles

--- src/components/Week/index.js
+++ src/components/Week/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import Day from '../Day';
+import { DialogOverlay, DialogContent } from '@reach/dialog';
+import '@reach/dialog/styles.css';
 import './style.scss';
 
 const Week = ({ recipeToAdd, done }) => {
 	const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 	const [activeDay, setActiveDay] = useState({ name: days[0], key: 0 });
 	const [isDesktop, setIsDesktop] = useState(false);
-	const [selectedDay, setSelectedDay] = useState(null);
+	const [selectedDay, setSelectedDay] = useState(null, { showDialog: true });
 
 	const setSize = () => {
 		window.innerWidth > 600 ? setIsDesktop(true) : setIsDesktop(false);
@@ -31,12 +33,21 @@ const Week = ({ recipeToAdd, done }) => {
 		<Fragment>
 			{recipeToAdd && (
 				<div>
-					What Day do you want to add it to?
-					{days.map((name, key) => (
-						<li key={key}>
-							<button onClick={key => setSelectedDay(name)}>{name}</button>
-						</li>
-					))}
+					<DialogOverlay>
+						<DialogContent
+							style={{
+								border: 'solid 5px hsla(0, 0%, 0%, 0.5)',
+								borderRadius: '10px',
+							}}
+						>
+							What Day do you want to add it to?
+							{days.map((name, key) => (
+								<li key={key}>
+									<button onClick={key => setSelectedDay(name)}>{name}</button>
+								</li>
+							))}
+						</DialogContent>
+					</DialogOverlay>
 				</div>
 			)}
 			<ul className="week">
