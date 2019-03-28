6143090636abeec2cf3f6998d5cdd9565905a8c1
fabiankaegy <fabian@arvernus.info>
2019-03-28 18:13:37 +0100

    Footer styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,8 +11,7 @@ import './style.scss';
 const Footer = () => {
 	return (
 		<div className="site-footer">
-			<p>A project by <a href="https://github.com/amberchunn">Amber Chunn</a> and <a href="https://github.com/fabankaegy">Faban Kägy</a>, developed for the <a href="https://javascriptforwp.com">javascriptforwp</a> hackathon early 2019.</p>
-			<p>You can find the source code on <a target="_" href="https://github.com/fabiankaegy/acfk-meal-planning">Github</a></p>
+			<p>A project by <a href="https://github.com/amberchunn">Amber Chunn</a> and <a href="https://github.com/fabankaegy">Faban Kägy</a>, developed for the <a href="https://javascriptforwp.com">javascriptforwp</a> hackathon early 2019.</p><p>You can find the source code on <a target="_" href="https://github.com/fabiankaegy/acfk-meal-planning">Github</a></p>
 		</div>
 	);
 };
